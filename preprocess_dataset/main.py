55e4b0505383d4388a38d094105f97ae15a0301f
simaataei <simaa.ataei@gmail.com>
2020-08-31 16:31:59 -0400

    final dataset extracted

--- preprocess_dataset/main.py
+++ preprocess_dataset/main.py
@@ -1,6 +1,7 @@
 from Bio import SeqIO
-original_data = list(SeqIO.parse("../Dataset/tcdb17feb2020.fasta", "fasta"))
 
+original_data = list(SeqIO.parse("../Dataset/tcdb17feb2020.fasta", "fasta"))
+import numpy as np
 
 # remove unacceptable amino acids
 '''
@@ -40,3 +41,84 @@ with open("../Dataset/normal_len_seq.fasta", "w") as handle:
     for rec in normal_len_dataset:
         SeqIO.write(rec, handle, "fasta")
 '''
+
+
+# Making a family map for data extraction
+'''
+family_map = {} #key:family number, value: id
+i = 0
+for item in original_data:
+     famil = item.id.split('|')[3].split('.')[0:3]
+     family = '.'.join(famil)
+
+     if family not in family_map:
+         family_map[family] = i
+         i += 1
+
+
+'''
+
+
+'''
+#extracting 30 random seqs for each family
+data = list(SeqIO.parse("../Dataset/normal_len_seq.fasta", "fasta"))
+selected_list_seqReq = []
+removed_list_seqReq = []
+for fm in family_map:
+     list_temp = []
+     for ro in data:
+         if '.'.join(ro.id.split('|')[-1].split('.')[0:3]) == fm:
+             list_temp.append(ro)
+     if len(list_temp) > 29:
+         new_random_list = list(np.random.permutation(list_temp))[0:30]
+         selected_list_seqReq.append(new_random_list)
+     else:
+         removed_list_seqReq.append(fm)
+
+print(removed_list_seqReq)
+print(len(removed_list_seqReq))
+print(selected_list_seqReq)
+
+with open("../Dataset/Selected30_families_tcdb.fasta", "w") as handle:
+     for rec in selected_list_seqReq:
+         SeqIO.write(rec, handle, "fasta")
+
+'''
+
+# making a subfamily map for data extraction
+subfamily_map = {} #key:subfamily number, value: id
+i = 0
+for item in original_data:
+     subfamil = item.id.split('|')[-1].split('.')[0:4]
+     subfamily = '.'.join(subfamil)
+
+     if subfamily not in subfamily_map:
+         subfamily_map[subfamily] = i
+         i += 1
+print(subfamily_map)
+
+#extracting 30 random seqs for each subfamily
+data = list(SeqIO.parse("../Dataset/normal_len_seq.fasta", "fasta"))
+selected_list_seqReq = []
+removed_list_seqReq = []
+for sfm in subfamily_map:
+     list_temp = []
+     for ro in data:
+         if '.'.join(ro.id.split('|')[-1].split('.')[0:4]) == sfm:
+             list_temp.append(ro)
+     if len(list_temp) > 29:
+         new_random_list = list(np.random.permutation(list_temp))[0:30]
+         selected_list_seqReq.append(new_random_list)
+     else:
+         removed_list_seqReq.append(sfm)
+
+print(removed_list_seqReq)
+print(len(removed_list_seqReq))
+print(len(selected_list_seqReq))
+
+with open("../Dataset/Selected30_subfamilies_tcdb.fasta", "w") as handle:
+     for rec in selected_list_seqReq:
+         SeqIO.write(rec, handle, "fasta")
+data = list(SeqIO.parse("../Dataset/Selected30_subfamilies_tcdb.fasta", "fasta"))
+print(len(data))
+
