6f547090fa9645ad2f7252bf37a2afeb2590959e
simaataei <simaa.ataei@gmail.com>
2020-10-08 16:43:21 -0400

    BLAST-evalation

--- classification/BLAST.py
+++ classification/BLAST.py
@@ -1,7 +1,7 @@
 from Bio import SeqIO
 import numpy as np
 import os
-
+from sklearn import metrics
 
 def blast(evalue, cv, data):
     test_data = []
@@ -31,8 +31,72 @@ def blast(evalue, cv, data):
         test_list = []
     s=2
 
-def evaluation(hits):
+def evaluation(neighbours,evalue, cv):
+
+    accuracy_list =[]
+    precision_list= []
+    f1_list = []
+    recall_list = []
+    for i in range(1, cv + 1):
+        true_family ={}
+        pred_family = {}
+        unclassified =[]
+        y_true = []
+        y_pred = []
+        test_sequences = list(
+            SeqIO.parse('../Results/BLAST/evalue-'+str(evalue)+'/testsetE'+str(evalue)+'Fold'+str(i)+'.fasta',
+                        'fasta'))
+        for item in test_sequences:
+            family = '.'.join(item.id.split('|')[3].split('.')[0:3])
+            true_family[item.id] = family
+
+        with open(f'../Results/BLAST/evalue-{evalue}/BLASTE{evalue}Fold{i}.list') as file:
+            results = file.readlines()
+            for result in results:
+                hit = result.split('\t')
+                if hit[0] not in pred_family.keys():
+                    hit_family = '.'.join(hit[1].split('|')[3].split('.')[0:3])
+                    pred_family[hit[0]] = hit_family
+                    a=1
+        for key in true_family.keys():
+            if key not in pred_family.keys():
+                unclassified.append(key)
+            else:
+                y_true.append(true_family[key])
+                y_pred.append(pred_family[key])
+        accuracy_list.append(metrics.accuracy_score(y_true, y_pred))
+        precision_list.append(metrics.precision_score(y_true, y_pred, average="macro"))
+        recall_list.append(metrics.recall_score(y_true, y_pred, average="macro"))
+        f1_list.append(metrics.f1_score(y_true, y_pred, average="macro"))
+    avg_accuracy = sum(accuracy_list)/cv
+    avg_precision = sum(precision_list) / cv
+    avg_recall = sum(recall_list) / cv
+    avg_f1 = sum(f1_list)/cv
+    return avg_accuracy,avg_precision,avg_recall,avg_f1
+
+
+
+
+
+
 
 
 data = list(SeqIO.parse("../Dataset/Selected30_families_tcdb2.fasta", "fasta"))
-blast(10,5,data)
+
+#11, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001, 0.000000001, 0.0000000001,
+evalues= [0.00000000001,0.000000000001,0.000000000001]
+for e in evalues:
+    blast(e,5,data)
+
+
+accuracy_list = []
+precision_list = []
+recall_list = []
+f1_list = []
+for e in evalues:
+    acc,precision,recall,f1 = evaluation(1,e,5)
+    accuracy_list.append(acc)
+    precision_list.append(precision)
+    recall_list.append(recall)
+    f1_list.append(f1)
+a=1
